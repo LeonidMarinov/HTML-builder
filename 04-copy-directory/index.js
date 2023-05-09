dd3022de2f74ed66710f597cc985d89477ba482d
leopold271 <lenya.m1998@gmail.com>
2023-05-09 19:09:32 +0300

    fix: fix 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,17 +6,22 @@ const copyDir = async () => {
   const destinationFolderPath = path.join(__dirname, 'files-copy')
   fs.mkdir(destinationFolderPath, {recursive: true});
   const filesToCopy = await fs.readdir(path.join(__dirname, 'files'))
+  let copiedFiles = ''
 
   for (let i = 0; i < filesToCopy.length; i++) {
     fs.copyFile(path.join(__dirname, 'files', filesToCopy[i]), path.join(__dirname, 'files-copy', filesToCopy[i]));
   }
 
-  const copiedFiles = await fs.readdir(path.join(__dirname, 'files-copy'))
+  copiedFiles = await fs.readdir(path.join(__dirname, 'files-copy'))
+
+  console.log('initial', filesToCopy.length)
+  console.log('copied', copiedFiles.length)
 
   for (let i = 0; i < copiedFiles.length; i++) {
-    if (!copiedFiles.includes(filesToCopy[i])) {
-      fs.unlink(path.join(__dirname, 'files-copy', copiedFiles[i]))
+    if (copiedFiles.length !== filesToCopy.length && typeof copiedFiles !== 'string') {
+      await fs.unlink(path.join(__dirname, 'files-copy', copiedFiles[i]))
     }
+    fs.copyFile(path.join(__dirname, 'files', filesToCopy[i]), path.join(__dirname, 'files-copy', filesToCopy[i]));
   }
 
 }
