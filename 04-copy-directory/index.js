5515c5407f70b943a1833aa6b64de287c1ab2f55
leopold271 <lenya.m1998@gmail.com>
2023-05-09 17:39:54 +0300

    fix: fix deletion of file case

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,7 +2,6 @@
 const fs = require('fs').promises;
 const path = require('path')
 
-
 const copyDir = async () => {
   const destinationFolderPath = path.join(__dirname, 'files-copy')
   fs.mkdir(destinationFolderPath, {recursive: true});
@@ -11,6 +10,15 @@ const copyDir = async () => {
   for (let i = 0; i < filesToCopy.length; i++) {
     fs.copyFile(path.join(__dirname, 'files', filesToCopy[i]), path.join(__dirname, 'files-copy', filesToCopy[i]));
   }
+
+  const copiedFiles = await fs.readdir(path.join(__dirname, 'files-copy'))
+
+  for (let i = 0; i < copiedFiles.length; i++) {
+    if (!copiedFiles.includes(filesToCopy[i])) {
+      fs.unlink(path.join(__dirname, 'files-copy', copiedFiles[i]))
+    }
+  }
+
 }
 
 copyDir();
