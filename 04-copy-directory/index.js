283edd35f212674cf3fb7e466d586397565b1ffc
leopold271 <lenya.m1998@gmail.com>
2023-05-10 09:42:14 +0300

    fix: fix 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,6 +3,9 @@ const fs = require('fs').promises;
 const path = require('path')
 
 const copyDir = async () => {
+
+  await fs.rm(path.join(__dirname, 'files-copy'), {recursive: true, force: true})
+
   const destinationFolderPath = path.join(__dirname, 'files-copy')
   fs.mkdir(destinationFolderPath, {recursive: true});
   const filesToCopy = await fs.readdir(path.join(__dirname, 'files'))
@@ -17,12 +20,12 @@ const copyDir = async () => {
   console.log('initial', filesToCopy.length)
   console.log('copied', copiedFiles.length)
 
-  for (let i = 0; i < copiedFiles.length; i++) {
-    if (copiedFiles.length !== filesToCopy.length && typeof copiedFiles !== 'string') {
-      await fs.unlink(path.join(__dirname, 'files-copy', copiedFiles[i]))
-    }
-    fs.copyFile(path.join(__dirname, 'files', filesToCopy[i]), path.join(__dirname, 'files-copy', filesToCopy[i]));
-  }
+  // for (let i = 0; i < copiedFiles.length; i++) {
+  //   if (copiedFiles.length !== filesToCopy.length && typeof copiedFiles !== 'string') {
+  //     await fs.unlink(path.join(__dirname, 'files-copy', copiedFiles[i]))
+  //   }
+  //   fs.copyFile(path.join(__dirname, 'files', filesToCopy[i]), path.join(__dirname, 'files-copy', filesToCopy[i]));
+  // }
 
 }
 
